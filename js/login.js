5f51083564bcdc1c7b661838922a8a23fe8f5bd6
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2023-12-24 02:26:06 +0600

    correction login and withdraw added

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
-document.getElementById('login-submit').addEventListener('click', function(){
+document.getElementById('btn-submit').addEventListener('click', function(){
     const emailField = document.getElementById('user-email');
-    const userEmail = emailField.ariaValueMax;
+    const userEmail = emailField.value;
     // get user password
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
